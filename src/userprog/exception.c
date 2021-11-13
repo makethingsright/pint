09cd168a11cd2687a0294f45a9d7a198049c85f2
younoknowme <31772419+younoknowme@users.noreply.github.com>
2021-11-12 19:48:10 -0500

    Update exception.c

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,6 +4,8 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "string.h"
+
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -147,10 +149,19 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
+  if(!user){
+     f->eip = (void (*) (void)) f->eax;
+     f->eax = 0xffffffff;
+     return ;
+  }
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
+ char *save_ptr;
+	char *name = strtok_r(thread_current()->name, " ", &save_ptr);
+        printf("%s: exit(%d)\n", name, -1);
+
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
@@ -158,4 +169,3 @@ page_fault (struct intr_frame *f)
           user ? "user" : "kernel");
   kill (f);
 }
-
