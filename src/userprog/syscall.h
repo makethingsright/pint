a353f63f88fddef02c7b5f18aa27e1b174da7762
younoknowme <31772419+younoknowme@users.noreply.github.com>
2021-11-12 19:49:14 -0500

    Update syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,5 +2,8 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
+void exit(int);
+int write(int fd, const void* buffer, unsigned size);
+void validate_address(void *);
 
 #endif /* userprog/syscall.h */
